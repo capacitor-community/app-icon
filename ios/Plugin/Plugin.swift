c1c56a7bd8d1936e6e52f5c370e50a306a078830
John Borges <jaborges@protonmail.ch>
2021-01-08 14:43:05 -0500

    feat: suppress popup warning

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -42,13 +42,24 @@ public class AppIcon: CAPPlugin {
         }
         
         DispatchQueue.main.sync {
-            UIApplication.shared.setAlternateIconName(iconName) { (error) in
-                // After app icon changed, print our error or success message
-                if let error = error {
-                    call.reject("App icon failed to due to \(error.localizedDescription)")
-                } else {
-                    call.resolve(["value": "App icon changed successfully."])
-                }
+//            UIApplication.shared.setAlternateIconName(iconName) { (error) in
+//                // After app icon changed, print our error or success message
+//                if let error = error {
+//                    call.reject("App icon failed to due to \(error.localizedDescription)")
+//                } else {
+//                    call.resolve(["value": "App icon changed successfully."])
+//                }
+//            }
+            
+            if UIApplication.shared.responds(to: #selector(getter: UIApplication.supportsAlternateIcons)) && UIApplication.shared.supportsAlternateIcons {
+                typealias setAlternateIconName = @convention(c) (NSObject, Selector, NSString?, @escaping (NSError) -> ()) -> ()
+                        
+                let selectorString = "_setAlternateIconName:completionHandler:"
+                        
+                let selector = NSSelectorFromString(selectorString)
+                let imp = UIApplication.shared.method(for: selector)
+                let method = unsafeBitCast(imp, to: setAlternateIconName.self)
+                method(UIApplication.shared, selector, iconName as NSString?, { _ in })
             }
         }
     }
