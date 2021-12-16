e5e22010c80fefe784ec40f653eb7b88bdb043fb
John Borges <jaborges@protonmail.ch>
2021-12-16 12:52:25 -0500

    build: cap v2 plugin fmt

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,21 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+
 export default {
   input: 'dist/esm/index.js',
-  output: [
-    {
-      file: 'dist/plugin.js',
-      format: 'iife',
-      name: 'capacitorAppCenter',
-      globals: {
-        '@capacitor/core': 'capacitorExports',
-      },
-      sourcemap: true,
-      inlineDynamicImports: true,
+  output: {
+    file: 'dist/plugin.js',
+    format: 'iife',
+    name: 'capacitorPlugin', // TODO: change this
+    globals: {
+      '@capacitor/core': 'capacitorExports',
     },
-    {
-      file: 'dist/plugin.cjs.js',
-      format: 'cjs',
-      sourcemap: true,
-      inlineDynamicImports: true,
-    },
+    sourcemap: true,
+  },
+  plugins: [
+    nodeResolve({
+      // allowlist of dependencies to bundle in
+      // @see https://github.com/rollup/plugins/tree/master/packages/node-resolve#resolveonly
+      resolveOnly: ['lodash'],
+    }),
   ],
-  external: ['@capacitor/core'],
 };
