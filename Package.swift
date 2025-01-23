f14603a4bbcbba3bd925d60836b627dda36314e4
John Borges <1888122+johnborges@users.noreply.github.com>
2025-01-22 20:16:44 -0500

    capacitor version 7 support (#58)
    Co-authored-by: Alexander Harding <noreply@harding.dev>

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CapacitorCommunityAppIcon",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "CapacitorCommunityAppIcon",
+            targets: ["AppIconPlugin"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main")
+    ],
+    targets: [
+        .target(
+            name: "AppIconPlugin",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm")
+            ],
+            path: "ios/Sources/AppIconPlugin"),
+        .testTarget(
+            name: "AppIconPluginTests",
+            dependencies: ["AppIconPlugin"],
+            path: "ios/Tests/AppIconPluginTests")
+    ]
+)
